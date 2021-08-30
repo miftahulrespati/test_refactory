2f357fc753fda38d3e9fd17a2fd04caa786db34c
miftahulrespati <63243218+miftahulrespati@users.noreply.github.com>
2021-08-30 18:38:22 +0700

    Update main.go

--- test_bootcamp_refactory/logic_test/4_nearest_fibonacci/main.go
+++ test_bootcamp_refactory/logic_test/4_nearest_fibonacci/main.go
@@ -1,29 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	a, b := 1, 1
 	arr := []int{15, 3, 1}
 	result := 0
-	output := 0
+	output := 0.
 
 	for _, v := range arr {
 		result += v
 	}
 
-	for a < result-1 {
+	for a < result {
 
 		b = a + b
 		a = b - a
 	}
 
-	if a < result {
-
-		output = (result - a)
-	} else if result < a {
-		output = (a - result)
-	}
+	output = (math.Abs(float64(a - result)))
 
 	fmt.Println("Fibonacci:", a)
 	fmt.Println("Result:", result)
